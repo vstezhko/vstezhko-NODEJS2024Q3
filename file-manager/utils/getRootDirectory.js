2254099e0248678cb96bcd8b7c2f253108f2a76c
Viktoria <viktoriastezhko@gmail.com>
2024-10-14 21:28:57 +0300

    fix: fix utils logic

--- file-manager/utils/getRootDirectory.js
+++ file-manager/utils/getRootDirectory.js
@@ -1,17 +1,7 @@
-const path = require('path');
 const os = require('os');
 
 function getRootDirectory() {
-    const currentPath = process.cwd();
-
-    if (os.platform() === 'win32') {
-        // On Windows, the root is the drive letter followed by ':\'
-        const drive = currentPath.split(path.sep)[0];
-        return `${drive}:\\`;
-    } else {
-        // On Unix-like systems, the root is '/'
-        return '/';
-    }
+    return os.homedir();
 }
 
 module.exports = getRootDirectory;
--- file-manager/utils/isWithinRoot.js
+++ file-manager/utils/isWithinRoot.js
@@ -1,17 +1,33 @@
 const path = require('path');
 const os = require('os');
 
-
 function isWithinRoot(targetPath, rootDirectory) {
+
     const normalizedTarget = path.resolve(targetPath);
     const normalizedRoot = path.resolve(rootDirectory);
+    console.log(normalizedRoot)
 
-    // On Windows, paths are case-insensitive
-    if (os.platform() === 'win32') {
-        return normalizedTarget.toLowerCase().startsWith(normalizedRoot.toLowerCase());
-    } else {
-        return normalizedTarget.startsWith(normalizedRoot);
+
+    const targetParts = normalizedTarget.split(path.sep);
+    const rootParts = normalizedRoot.split(path.sep);
+
+    console.log(targetParts)
+    console.log(rootParts)
+
+
+    for (let i = 0; i < rootParts.length; i++) {
+        if (os.platform() === 'win32') {
+            if (targetParts[i].toLowerCase() !== rootParts[i].toLowerCase()) {
+                return false;
+            }
+        } else {
+            if (targetParts[i] !== rootParts[i]) {
+                return false;
+            }
+        }
     }
+
+    return true;
 }
 
 module.exports = isWithinRoot;
